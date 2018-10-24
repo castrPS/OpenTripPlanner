3c8b266eac97151f7b0445a6716e3086c6e7c127
rprados <rprados@motorola.com>
2018-10-24 12:13:33 -0300

    WithGraph finalizada

--- src/main/java/org/opentripplanner/model/json_serialization/WithGraph.java
+++ src/main/java/org/opentripplanner/model/json_serialization/WithGraph.java
@@ -3,16 +3,18 @@ package org.opentripplanner.model.json_serialization;
 import org.opentripplanner.routing.graph.Graph;
 
 public class WithGraph {
-    private Graph graph;
-    private Object object;
+    private /*@ spec_public @*/  Graph graph;
+    private /*@ spec_public @*/ Object object;
     public WithGraph(Graph graph, Object object) {
         this.graph = graph;
         this.object = object;
     }
+    //@ requires graph != null;
     public Graph getGraph() {
         return graph;
     }
     
+    //@ requires object != null;
     public Object getObject() {
         return object;
     }
